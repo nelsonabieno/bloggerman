ef574a9b81fe9d93988e5e5c13ffedd1d908c3cc
nelsonabieno <=>
2020-01-06 12:46:47 +0100

    allow only authenticated users to access the user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   get 'pages/home'
   devise_for :users
+
   resources :users do
     resources :posts do
       resources :comments
     end
   end
+
   root to: redirect('/pages/home')
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
