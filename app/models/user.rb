e7e208231943dd1fa262135f89696465d3e8754c
nelsonabieno <=>
2020-01-08 13:18:50 +0100

    write user controller and model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # has_secure_password
   has_many :posts, dependent: :destroy
-  # validates :nickname, length: { minimum: 5 }, presence: true
-  # validates :email,
-  #           format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
-  #           presence: true,
-  #           uniqueness: true
-  # validates :password_digest, length: { minimum: 5 }, presence:true
-  # validates :password, presence:true
-  # validates :password, confirmation: { case_sensitive: true }
 end
