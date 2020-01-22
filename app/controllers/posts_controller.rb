a1008f62dc495a45a294ac69694528501dfec596
nelsonabieno <=>
2020-01-22 14:51:41 +0100

    add show template and response format handling for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,20 @@
 class PostsController < ApplicationController
-  before_action :find_user, only: [:new, :index,:edit]
-  before_action :find_user_post, only: [:update,:destroy]
+  before_action :find_user, only: [:create, :new, :index,:edit, :update, :show]
+  before_action :find_user_post, only: [:update,:destroy, :show]
 
   def index
     @user
+    response_handler @user.posts
   end
-
 
   def new
     @post = Post.new
+  end
+
+  def show
+    @user
+    @post = @user.posts.find(params[:id])
+    response_handler @user_post
   end
 
   def create
@@ -17,10 +23,16 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:success] = 'Post successfully created'
-      redirect_to user_posts_path
+      respond_to do |format|
+        format.json { response_handler @post  }
+        format.html { redirect_to user_posts_path }
+      end
     else
       flash[:error] = @post.errors.full_messages
-      render 'posts/new'
+      respond_to do |format|
+        format.json { render json: { error: flash[:error] } }
+        format.html { render 'posts/new' }
+      end
     end
   end
 
@@ -29,23 +41,36 @@ class PostsController < ApplicationController
 
     if @post.nil?
       flash[:error] = ['Sorry We couldn\'t find the post for this user']
+      response_handler flash[:error]
       redirect_to user_posts_path
     end
   end
 
   def update
+    @post = @user.posts.find(params[:id])
+
     if @user_post.update(post_params)
       flash[:success] = 'Post successfully updated'
-      redirect_to user_posts_path
+      respond_to do |format|
+        format.json { response_handler @user_post  }
+        format.html { redirect_to user_posts_path }
+      end
+    else
+      flash[:error] = @user_post.errors.full_messages
+      respond_to do |format|
+        format.json { render json: { error: flash[:error] } }
+        format.html { render 'posts/edit' }
+      end
     end
-
-    #TODO else block
   end
 
   def destroy
     if @user_post.destroy
       flash[:success] = 'Post successfully deleted'
-      redirect_to user_posts_path
+      respond_to do |format|
+        format.json { render json: flash[:success] }
+        format.html { redirect_to user_posts_path }
+      end
     end
   end
 
@@ -56,9 +81,9 @@ class PostsController < ApplicationController
 
     if @user_post.nil?
       flash[:error] = ['Sorry We couldn\'t find the post for this user']
+      response_handler flash[:error]
       redirect_to user_posts_path
     end
-
   end
 
   def find_user
@@ -67,7 +92,7 @@ class PostsController < ApplicationController
 
   def response_handler data
     respond_to do |format|
-      format.json { render json: data }
+      format.json { render json: data  and return}
       format.html
     end
   end
